6e42af2efede1d653260229f9021cbb9cd4fba0d
Jukka Jylänki <jujjyl@gmail.com>
2012-05-28 23:35:25 +0300

    Avoid Windows.h Polygon() function collision with class Polygon when including GeometryAll.h.

--- src/Geometry/GeometryAll.h
+++ src/Geometry/GeometryAll.h
@@ -18,6 +18,25 @@
 
 #pragma once
 
+#include "Math/MathBuildConfig.h"
+#include "Math/MathNamespace.h"
+
+#ifdef WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+// Windows GDI has a global function named Polygon. I am not renaming my Polygon object just for its sake, especially since
+// I'm not expecting anyone to co-use this library with GDI. Kill the Polygon function from Windows. and force-include 
+// Windows.h here to erase that function signature.
+
+// If this is an issue for you, do the following things to use MathGeoLib from behind a namespace:
+// 1. Enable MATH_ENABLE_NAMESPACE and disable MATH_AUTO_USE_NAMESPACE in MathBuildConfig.h
+// 2. Comment out the following three lines.
+#define Polygon Polygon_unused
+#include <Windows.h>
+#undef Polygon
+#endif
+
 #include "AABB.h"
 #include "AABB2D.h"
 #include "Capsule.h"
@@ -37,3 +56,4 @@
 #include "Sphere.h"
 #include "Triangle.h"
 #include "TriangleMesh.h"
+#include "GeomType.h"
