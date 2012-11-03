4957648f5acb3fa0124cdd7fa4094d228c4271f1
Jukka Jylänki <jujjyl@gmail.com>
2012-11-04 01:52:32 +0200

    Build with /W4 by default. Added vs2010-64bit cmake build folder.

--- CommonOptions.cmake
+++ CommonOptions.cmake
@@ -16,3 +16,15 @@ if (EMSCRIPTEN)
 	SET(CMAKE_FIND_LIBRARY_PREFIXES "")
 	SET(CMAKE_FIND_LIBRARY_SUFFIXES ".bc")
 endif()
+
+if(MSVC)
+  # Force to always compile with W4
+  if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
+    string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+  else()
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
+  endif()
+elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
+  # Update if necessary
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-long-long -pedantic")
+endif()
