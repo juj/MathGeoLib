c766f131c546a30efde63603999ed4680a814a46
Jukka Jylänki <jujjyl@gmail.com>
2012-11-04 12:25:28 +0200

    Ignore warning on GCC for using C99 variadic macros.

--- CommonOptions.cmake
+++ CommonOptions.cmake
@@ -26,5 +26,5 @@ if(MSVC)
   endif()
 elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
   # Update if necessary
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-long-long -pedantic")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wno-long-long -pedantic -Wno-variadic-macros")
 endif()
