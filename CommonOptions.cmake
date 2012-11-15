123f172ef6cbca46795da2ada310c5435f1ad5ee
Jukka Jylänki <jujjyl@gmail.com>
2012-11-15 22:45:16 +0200

    Don't pass /arch:SSE2 to Visual Studio 2012, since it's always guaranteed and the flag no longer exists.

--- CommonOptions.cmake
+++ CommonOptions.cmake
@@ -1,5 +1,8 @@
 if (WIN32 AND NOT MINGW)
-	add_definitions(/EHsc /arch:SSE2 -D_CRT_SECURE_NO_WARNINGS)
+	add_definitions(/EHsc -D_CRT_SECURE_NO_WARNINGS)
+	if (NOT MSVC11) # Visual Studio 2012 always has SSE2 enabled, so the command line option no longer exists.
+		add_definitions(/arch:SSE2)
+	endif()
 endif()
 
 if (EMSCRIPTEN)
