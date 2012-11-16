573815a5e05143687f113080a4e87f7effcf57bb
Jukka Jylänki <jujjyl@gmail.com>
2012-11-17 00:57:01 +0200

    Hide the ' The function xxx is never used.' warning from cppcheck since as a generic utility library, most of the functions are not used by the library itself.

--- cppcheck/run_cppcheck.bat
+++ cppcheck/run_cppcheck.bat
@@ -1,9 +1,10 @@
-:: The warning 'Conversion of string literal "xxx" to bool always evaluates to true.' is suppressed because of the commonly used idiom 'assert(condition && "error message");'
-:: The warning 'Member variable 'ClassName::memberName' is not initialized in the constructor.' is suppressed because MathGeoLib doesn't commonly initialize members in ctors (performance).
+:: The warning incorrectStringBooleanError: 'Conversion of string literal "xxx" to bool always evaluates to true.' is suppressed because of the commonly used idiom 'assert(condition && "error message");'
+:: The warning uninitMemberVar: 'Member variable 'ClassName::memberName' is not initialized in the constructor.' is suppressed because MathGeoLib doesn't commonly initialize members in ctors (performance).
+:: The warning unusedFunction: 'The function 'funcName' is never used.' is suppressed because as a generic utility library, most of the functions are not used by the library itself.
 
 "C:\Program Files (x86)\Cppcheck\cppcheck" --version
 
-"C:\Program Files (x86)\Cppcheck\cppcheck" --enable=all --suppress=uninitMemberVar --suppress=incorrectStringBooleanError --force ../src
+"C:\Program Files (x86)\Cppcheck\cppcheck" --enable=all --suppress=unusedFunction --suppress=uninitMemberVar --suppress=incorrectStringBooleanError --force ../src
 
 pause
 
