bb0cf0f3abe94d1743145bf454d141e81c3c4dc4
Jukka Jylänki <jujjyl@gmail.com>
2012-12-03 22:20:39 +0200

    Add mingw32-make and gcc version print to mingw-make.bat for buildbot.

--- mingw/mingw-make.bat
+++ mingw/mingw-make.bat
@@ -1,2 +1,11 @@
+@echo off
+SET OLDPATH=%PATH%
+SET PATH=%PATH:cygwin=cygwin_unused%
+echo mingw32-make --version
+mingw32-make --version
+echo.
+echo gcc --version
+gcc --version
 mingw32-make
+SET PATH=%OLDPATH%
 pause
