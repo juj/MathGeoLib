00b104a24567c211a8fda7c1df8a899f79dcdac1
Jukka Jylänki <jujjyl@gmail.com>
2012-12-04 15:39:13 +0200

    Suppress 'noConstructor' style diagnostic from cppcheck.

--- cppcheck/run_cppcheck.bat
+++ cppcheck/run_cppcheck.bat
@@ -1,10 +1,11 @@
 :: The warning incorrectStringBooleanError: 'Conversion of string literal "xxx" to bool always evaluates to true.' is suppressed because of the commonly used idiom 'assert(condition && "error message");'
 :: The warning uninitMemberVar: 'Member variable 'ClassName::memberName' is not initialized in the constructor.' is suppressed because MathGeoLib doesn't commonly initialize members in ctors (performance).
 :: The warning unusedFunction: 'The function 'funcName' is never used.' is suppressed because as a generic utility library, most of the functions are not used by the library itself.
+:: The warning noConstructor: 'The class 'classname' does not have a constructor is suppressed because MathGeoLib uses the keyword 'class' for POD-like objects as well.
 
 "C:\Program Files (x86)\Cppcheck\cppcheck" --version
 
-"C:\Program Files (x86)\Cppcheck\cppcheck" -UMATH_QT_INTEROP -I../src -rp=../src --enable=all --suppress=unusedFunction --suppress=uninitMemberVar --suppress=incorrectStringBooleanError --force ../src
+"C:\Program Files (x86)\Cppcheck\cppcheck" -UMATH_QT_INTEROP -I../src -rp=../src --enable=all --suppress=unusedFunction --suppress=noConstructor --suppress=uninitMemberVar --suppress=incorrectStringBooleanError --force ../src
 
 pause
 
