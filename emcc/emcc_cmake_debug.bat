4aff71f82c71aa39e1c4fdffb147e215fc34d6ec
Jukka Jylänki <jujjyl@gmail.com>
2012-12-08 12:03:58 +0200

    Delete emcc toolchain cmake file, as it now comes bundled with emscripten.

--- emcc/emcc_cmake_debug.bat
+++ emcc/emcc_cmake_debug.bat
@@ -1,1 +1,2 @@
 cmake -DCMAKE_TOOLCHAIN_FILE=emcc_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" ..
+pause
--- emcc/emcc_cmake_release.bat
+++ emcc/emcc_cmake_release.bat
@@ -1,1 +1,2 @@
 cmake -DCMAKE_TOOLCHAIN_FILE=emcc_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ..
+pause
