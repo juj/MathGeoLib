e399fe20ad84725f716c0f25e4e31bed46f4b6de
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2012-12-15 11:57:59 +0100

    Added build script for building release mode tests on linux.

---
+++ linux/cmake_release-tests.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm CMakeCache.txt
+cmake -DMATH_TESTS_EXECUTABLE=1 -DLINUX=1 -DCMAKE_BUILD_TYPE=Release -DBUILD_FOR_GCOV=1 -G "Unix Makefiles" ..
+
+echo "make --version"
+make --version
+
+echo " "
+
+echo "gcc --version"
+gcc --version
