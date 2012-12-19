9d427bf4396c2e33bfc024c672b439f0bd311e7a
Jukka Jylänki <jujjyl@gmail.com>
2012-12-19 22:58:53 +0200

    Added serialization tests for float2/float3/float4.

--- tests/SerializationTests.cpp
+++ tests/SerializationTests.cpp
@@ -5,6 +5,83 @@
 #include "myassert.h"
 #include "TestRunner.h"
 
+void TestFloat2FromString()
+{
+	const char *locales[] = { "C", "en", "fi" }; // From http://www.loc.gov/standards/iso639-2/php/code_list.php
+
+	for(int i = -1; i < (int)(sizeof(locales)/sizeof(locales[0])); ++i)
+	{
+		if (i != -1)
+			setlocale(LC_ALL, locales[i]);
+		assert(float2::FromString("-1.0000, +2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000, +2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000, +2)").Equals(float2(-1.0000,2)));
+
+		assert(float2::FromString("-1.0000 ; +2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000 ; +2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000 ; +2)").Equals(float2(-1.0000,2)));
+
+		assert(float2::FromString("-1.0000,+2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000,+2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000,+2)").Equals(float2(-1.0000,2)));
+
+		assert(float2::FromString("-1.0000 +2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000 +2").Equals(float2(-1.0000,2)));
+		assert(float2::FromString("(-1.0000 +2)").Equals(float2(-1.0000,2)));
+	}
+}
+
+void TestFloat3FromString()
+{
+	const char *locales[] = { "C", "en", "fi" }; // From http://www.loc.gov/standards/iso639-2/php/code_list.php
+
+	for(int i = -1; i < (int)(sizeof(locales)/sizeof(locales[0])); ++i)
+	{
+		if (i != -1)
+			setlocale(LC_ALL, locales[i]);
+		assert(float3::FromString("+2, 3.1, -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2, 3.1, -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2, 3.1, -4)").Equals(float3(2,3.1f,-4)));
+
+		assert(float3::FromString("+2; 3.1; -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2; 3.1; -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2; 3.1; -4)").Equals(float3(2,3.1f,-4)));
+
+		assert(float3::FromString("+2,3.1, -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2,3.1, -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2,3.1, -4)").Equals(float3(2,3.1f,-4)));
+
+		assert(float3::FromString("+2 3.1  -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2 3.1  -4").Equals(float3(2,3.1f,-4)));
+		assert(float3::FromString("(+2 3.1  -4)").Equals(float3(2,3.1f,-4)));
+	}
+}
+void TestFloat4FromString()
+{
+	const char *locales[] = { "C", "en", "fi" }; // From http://www.loc.gov/standards/iso639-2/php/code_list.php
+
+	for(int i = -1; i < (int)(sizeof(locales)/sizeof(locales[0])); ++i)
+	{
+		if (i != -1)
+			setlocale(LC_ALL, locales[i]);
+		assert(float4::FromString("1, +2, 3.1, -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1, +2, 3.1, -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1, +2, 3.1, -4)").Equals(float4(1,2,3.1f,-4)));
+
+		assert(float4::FromString("1 ; +2; 3.1; -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1 ; +2; 3.1; -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1 ; +2; 3.1; -4)").Equals(float4(1,2,3.1f,-4)));
+
+		assert(float4::FromString("1,+2,3.1, -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1,+2,3.1, -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1,+2,3.1, -4)").Equals(float4(1,2,3.1f,-4)));
+
+		assert(float4::FromString("1 +2 3.1  -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1 +2 3.1  -4").Equals(float4(1,2,3.1f,-4)));
+		assert(float4::FromString("(1 +2 3.1  -4)").Equals(float4(1,2,3.1f,-4)));
+	}
+}
+
 void TestQuatFromString()
 {
 	const char *locales[] = { "C", "en", "fi" }; // From http://www.loc.gov/standards/iso639-2/php/code_list.php
@@ -33,5 +110,8 @@ void TestQuatFromString()
 
 void AddSerializationTests()
 {
+	AddTest("Float2::FromString", TestFloat2FromString);
+	AddTest("Float3::FromString", TestFloat3FromString);
+	AddTest("Float4::FromString", TestFloat4FromString);
 	AddTest("Quat::FromString", TestQuatFromString);
 }
