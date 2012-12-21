720e4278f66867f694fbd08173ed81b1b70284c9
Jukka Jylänki <jujjyl@gmail.com>
2012-12-21 23:15:42 +0200

    Added new line-related unit tests.

--- tests/LineTests.cpp
+++ tests/LineTests.cpp
@@ -86,9 +86,106 @@ void TestLineLineSegmentClosestPoint()
 	assert(closestPointA.Distance(closestPointB) <= a.Distance(b.b));
 }
 
+void TestRayRayClosestPoint()
+{
+	float3 pt = float3::RandomBox(rng, -float3(SCALE,SCALE,SCALE), float3(SCALE,SCALE,SCALE));
+	float3 pt2 = float3::RandomBox(rng, -float3(SCALE,SCALE,SCALE), float3(SCALE,SCALE,SCALE));
+	Ray a = RandomRayContainingPoint(pt);
+	Ray b = RandomRayContainingPoint(pt2);
+
+	float d, d2;
+	float3 closestPointA = a.ClosestPoint(b, &d, &d2);
+	assert(closestPointA.Equals(a.GetPoint(d), 1e-2f));
+	float3 closestPointB = b.GetPoint(d2);
+	float D, D2;
+	float3 closestPointB2 = b.ClosestPoint(a, &D, &D2);
+	assert(EqualAbs(d, D2));
+	assert(EqualAbs(D, d2));
+	assert(closestPointB.Equals(closestPointB2, 1e-2f));
+	float3 closestPointA2 = a.GetPoint(D2);
+	assert(closestPointA.Equals(closestPointA2, 1e-2f));
+
+	assert(closestPointA.Distance(closestPointB) <= pt.Distance(pt2));
+	assert(EqualAbs(a.Distance(b), closestPointA.Distance(closestPointB)));
+
+	assert(closestPointA.Distance(closestPointB) <= closestPointA.Distance(b.pos));
+	assert(closestPointA.Distance(closestPointB) <= a.Distance(b.pos));
+
+	assert(closestPointA.Distance(closestPointB) <= closestPointB.Distance(a.pos));
+	assert(closestPointA.Distance(closestPointB) <= b.Distance(a.pos));
+}
+
+void TestRayLineSegmentClosestPoint()
+{
+	float3 pt = float3::RandomBox(rng, -float3(SCALE,SCALE,SCALE), float3(SCALE,SCALE,SCALE));
+	float3 pt2 = float3::RandomBox(rng, -float3(SCALE,SCALE,SCALE), float3(SCALE,SCALE,SCALE));
+	Ray a = RandomRayContainingPoint(pt);
+	LineSegment b = RandomLineSegmentContainingPoint(pt2);
+
+	float d, d2;
+	float3 closestPointA = a.ClosestPoint(b, &d, &d2);
+	assert(closestPointA.Equals(a.GetPoint(d), 1e-2f));
+	float3 closestPointB = b.GetPoint(d2);
+	float D, D2;
+	float3 closestPointB2 = b.ClosestPoint(a, &D, &D2);
+	assert(EqualAbs(d, D2));
+	assert(EqualAbs(D, d2));
+	assert(closestPointB.Equals(closestPointB2, 1e-2f));
+	float3 closestPointA2 = a.GetPoint(D2);
+	assert(closestPointA.Equals(closestPointA2, 1e-2f));
+
+	assert(closestPointA.Distance(closestPointB) <= pt.Distance(pt2));
+	assert(EqualAbs(a.Distance(b), closestPointA.Distance(closestPointB)));
+
+	assert(closestPointA.Distance(closestPointB) <= closestPointA.Distance(b.a));
+	assert(closestPointA.Distance(closestPointB) <= closestPointA.Distance(b.b));
+	assert(closestPointA.Distance(closestPointB) <= a.Distance(b.a));
+	assert(closestPointA.Distance(closestPointB) <= a.Distance(b.b));
+
+	assert(closestPointA.Distance(closestPointB) <= closestPointB.Distance(a.pos));
+	assert(closestPointA.Distance(closestPointB) <= b.Distance(a.pos));
+}
+
+void TestLineSegmentLineSegmentClosestPoint()
+{
+	float3 pt = float3::RandomBox(rng, -float3(SCALE,SCALE,SCALE), float3(SCALE,SCALE,SCALE));
+	float3 pt2 = float3::RandomBox(rng, -float3(SCALE,SCALE,SCALE), float3(SCALE,SCALE,SCALE));
+	LineSegment a = RandomLineSegmentContainingPoint(pt);
+	LineSegment b = RandomLineSegmentContainingPoint(pt2);
+
+	float d, d2;
+	float3 closestPointA = a.ClosestPoint(b, &d, &d2);
+	assert(closestPointA.Equals(a.GetPoint(d), 1e-2f));
+	float3 closestPointB = b.GetPoint(d2);
+	float D, D2;
+	float3 closestPointB2 = b.ClosestPoint(a, &D, &D2);
+	assert(EqualAbs(d, D2));
+	assert(EqualAbs(D, d2));
+	assert(closestPointB.Equals(closestPointB2, 1e-2f));
+	float3 closestPointA2 = a.GetPoint(D2);
+	assert(closestPointA.Equals(closestPointA2, 1e-2f));
+
+	assert(closestPointA.Distance(closestPointB) <= pt.Distance(pt2));
+	assert(EqualAbs(a.Distance(b), closestPointA.Distance(closestPointB)));
+
+	assert(closestPointA.Distance(closestPointB) <= closestPointA.Distance(b.a));
+	assert(closestPointA.Distance(closestPointB) <= closestPointA.Distance(b.b));
+	assert(closestPointA.Distance(closestPointB) <= a.Distance(b.a));
+	assert(closestPointA.Distance(closestPointB) <= a.Distance(b.b));
+
+	assert(closestPointA.Distance(closestPointB) <= closestPointB.Distance(a.a));
+	assert(closestPointA.Distance(closestPointB) <= closestPointB.Distance(a.b));
+	assert(closestPointA.Distance(closestPointB) <= b.Distance(a.a));
+	assert(closestPointA.Distance(closestPointB) <= b.Distance(a.b));
+}
 void AddLineTests()
 {
 	AddRandomizedTest("Line-Line ClosestPoint", TestLineLineClosestPoint);
 	AddRandomizedTest("Line-Ray ClosestPoint", TestLineRayClosestPoint);
 	AddRandomizedTest("Line-LineSegment ClosestPoint", TestLineLineSegmentClosestPoint);
+
+	AddRandomizedTest("Ray-Ray ClosestPoint", TestRayRayClosestPoint);
+	AddRandomizedTest("Ray-LineSegment ClosestPoint", TestRayLineSegmentClosestPoint);
+
+	AddRandomizedTest("LineSegment-LineSegment ClosestPoint", TestLineSegmentLineSegmentClosestPoint);
 }
