82e8c2a14accb14a0593675162046b2fbb7e49c4
Jukka Jylänki <jujjyl@gmail.com>
2012-12-22 17:21:23 +0200

    Expand pragma(warning(disable:4127)) to cover whole multi-line macro block so that assert(xxx && "message"); pattern won't trigger an useless warning.

--- src/Log.h
+++ src/Log.h
@@ -18,16 +18,20 @@
 #pragma once
 
 // From http://cnicholson.net/2009/03/stupid-c-tricks-dowhile0-and-c4127/
-#define MULTI_LINE_MACRO_BEGIN do {
-
 #ifdef _MSC_VER
-#define MULTI_LINE_MACRO_END \
+#define MULTI_LINE_MACRO_BEGIN do { \
 	__pragma(warning(push)) \
-	__pragma(warning(disable:4127)) \
+	__pragma(warning(disable:4127))
+
+#define MULTI_LINE_MACRO_END \
 	} while(0) \
 	__pragma(warning(pop))
+
 #else
+
+#define MULTI_LINE_MACRO_BEGIN do {
 #define MULTI_LINE_MACRO_END } while(0)
+
 #endif
 
 /// A bitfield type that describes single or multiple log channels (each bit represents a channel).
