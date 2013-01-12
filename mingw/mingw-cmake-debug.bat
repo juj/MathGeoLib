ed8cfdc8a5a90f1368d810197a4529b2d03cc07c
Jukka Jylänki <jujjyl@gmail.com>
2013-01-13 01:15:35 +0200

    For MinGW cmake invocations, remove TortoiseGit from path since it contains sh.exe which makes mingw32-make go wild.

--- mingw/mingw-cmake-debug.bat
+++ mingw/mingw-cmake-debug.bat
@@ -1,5 +1,6 @@
 set OLDPATH=%PATH%
 set PATH=%PATH:cygwin=cygwin_unused%
+set PATH=%PATH:git=git_unused%
 call g++ --version
 del CMakeCache.txt
 cmake -DMINGW=1 -G "MinGW Makefiles" .. %1 %2 %3 %4 %5 %6 %7 %8 %9
