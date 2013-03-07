fce51bb08e50b22d1e0afd604cf46a7248156f18
Jukka Jylänki <jujjyl@gmail.com>
2013-03-07 21:51:40 +0200

    Suppress ConfigurationNotChecked from cppcheck.

--- cppcheck/run_cppcheck.bat
+++ cppcheck/run_cppcheck.bat
@@ -2,10 +2,11 @@
 :: The warning uninitMemberVar: 'Member variable 'ClassName::memberName' is not initialized in the constructor.' is suppressed because MathGeoLib doesn't commonly initialize members in ctors (performance).
 :: The warning unusedFunction: 'The function 'funcName' is never used.' is suppressed because as a generic utility library, most of the functions are not used by the library itself.
 :: The warning noConstructor: 'The class 'classname' does not have a constructor is suppressed because MathGeoLib uses the keyword 'class' for POD-like objects as well.
+:: The warning ConfigurationNotChecked: 'Skipping configuration 'Complex' because it seems to be invalid. Use -D if you want to check it.' is suppressed because cppcheck runs through combinations that are not needed.
 
 "C:\Program Files (x86)\Cppcheck\cppcheck" --version
 
-"C:\Program Files (x86)\Cppcheck\cppcheck" --template "{file}({line}): ({severity}) ({id}): {message}" -UMATH_QT_INTEROP -I../src -rp=../src --enable=all --suppress=unusedFunction --suppress=noConstructor --suppress=uninitMemberVar --suppress=incorrectStringBooleanError --error-exitcode=1 --force ../src
+"C:\Program Files (x86)\Cppcheck\cppcheck" --template "{file}({line}): ({severity}) ({id}): {message}" -UMATH_QT_INTEROP -I../src -rp=../src --enable=all --suppress=unusedFunction --suppress=noConstructor --suppress=uninitMemberVar --suppress=ConfigurationNotChecked --suppress=incorrectStringBooleanError --error-exitcode=1 --force ../src
 
 pause
 
