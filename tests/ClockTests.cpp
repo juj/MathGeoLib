e4197c2b5b1e1a29886d987b810a00157319f538
Jukka Jylänki <jujjyl@gmail.com>
2013-08-14 01:04:48 +0300

    Remove redundant assert() on unsigned variable in test.

--- tests/ClockTests.cpp
+++ tests/ClockTests.cpp
@@ -11,7 +11,6 @@ TEST(MonotonousClock)
 	{
 		tick_t now = Clock::Tick();
 		tick_t diff = Clock::TicksInBetween(now, prev);
-		assert(diff >= 0);
 		assert(diff < Clock::TicksPerSec());
 		prev = now;
 		maxDiff = Max(diff, maxDiff);
