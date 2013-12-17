99bff5a6e61e77114342bae818cc44e6c1e73899
Jukka Jylänki <jujjyl@gmail.com>
2013-12-17 11:32:37 +0200

    Tinker with SSE version of u32->string conversion.

--- tests/ConversionTests.cpp
+++ tests/ConversionTests.cpp
@@ -3,13 +3,18 @@
 #include <stdint.h>
 #include <string.h>
 
+#include "../src/MathBuildConfig.h"
 #include "../src/MathGeoLib.h"
 #include "../src/Math/myassert.h"
 #include "TestRunner.h"
 #include "TestData.h"
 #include <cmath>
 
+#include "../src/Math/SSEMath.h"
+
 using namespace TestData;
+
+char int_to_string[64];
 
 // Test if a manually written int->string conversion beats the built-in variants. Results from the benchmarks below:
 // U32ToString - 23.488 nsecs
@@ -30,7 +35,7 @@ int U32ToString(u32 i, char *str)
 		i = ni;
 	}
 	*s = '\0';
-	int len = s - str;
+	ptrdiff_t len = s - str;
 	for(int i = 0; i < len/2; ++i)
 	{
 		char ch = str[i];
@@ -38,8 +43,110 @@ int U32ToString(u32 i, char *str)
 		str[len-1-i] = ch;
 	}
 
-	return s - str;
+	return (int)(s - str);
 }
+
+#ifdef MATH_SSE
+
+#ifdef _MSC_VER
+#define LOAD_TO_M128(dst, integer) dst.m128i_i32[0] = integer;
+#else
+#define LOAD_TO_M128(dst, integer) dst = _mm_loadu_si32(&integer)
+#endif
+
+FORCE_INLINE __m128i INT_TO_M128(int i)
+{
+	__m128i v = _mm_setzero_si128();
+	LOAD_TO_M128(v, i);
+	return v;
+}
+
+FORCE_INLINE int M128_TO_INT16(__m128i i)
+{
+	return i.m128i_i16[0];
+}
+
+static const __m128i one_hundredmth = INT_TO_M128((int)2882303761 /*42.94967295*65536*32*32 */);
+static const __m128i hundredm = INT_TO_M128(100000000);
+static const __m128i one_tenthousandth = _mm_set1_epi32((int)3518437209 /*429496.7295*8192 */);
+static const __m128i tenthousand = _mm_set1_epi32(10000);
+static const __m128i one_hundredth = _mm_set1_epi16((short)41944 /*655.36*64 */);
+static const __m128i hundred = _mm_set1_epi16(100);
+static const __m128i one_tenth = _mm_set1_epi16((short)52429 /* == 8 * 6553.6 */);
+static const __m128i ten = _mm_set1_epi16(10);
+static const __m128i zerochar = _mm_set1_epi8('0');
+
+// Warning: This is about 5 times slower than the C version! Better to just use the C version.
+int U32ToString_SSE(u32 i, char *str)
+{
+	// Max u32: 4294967295 ~ 4e9
+
+	// 4e9 / 1e8 -> [ 42 (64), 94967295 (64) ]
+	//     / 1e4 -> [ .. (32), 42 (32), 9496 (32), 7295 (32) ]
+	//     / 1e2 -> [ (16), (16), (16), 42 (16), 94 (16), 96 (16), 72 (16), 95 (16) ]
+	//     / 10  -> [ 
+
+	__m128i v = INT_TO_M128((int)i);
+
+	// 1/100000000 ~~ 42.94967296 / 4294967296
+	__m128i hi = _mm_srli_epi64(_mm_mul_epu32(v, one_hundredmth), 26+32);
+	__m128i lo = _mm_sub_epi32(v, _mm_mullo_epi32(hi, hundredm));
+
+	__m128i msb = _mm_slli_si128(hi, 6); // [42, 00, 00, 00, 00], or two most significant digits as 16-bit
+	v = lo; // 0 - 94967295 in 32-bit
+	// 1/10000 ~~ 429496.7296 / 4294967296
+	hi = _mm_srli_epi64(_mm_mul_epu32(v, one_tenthousandth), 13+32);
+	lo = _mm_sub_epi32(v, _mm_mullo_epi32(hi, tenthousand));
+
+	v = _mm_unpacklo_epi16(hi, lo); // [7295, 9496] in 16-bit
+
+	hi = _mm_srli_epi16(_mm_mulhi_epu16(v, one_hundredth), 6);
+	lo = _mm_sub_epi16(v, _mm_mullo_epi16(hi, hundred));
+
+	v = _mm_unpacklo_epi16(hi, lo);  // [95, 72, 96, 94] in 16-bit
+	v = _mm_unpacklo_epi64(msb, v); // [94, 96, 72, 95, 42, 00, 00, 00] in 16-bit
+	hi = _mm_srli_epi16(_mm_mulhi_epu16(v, one_tenth), 3);
+	lo = _mm_sub_epi16(v, _mm_mullo_epi16(hi, ten));
+
+	lo = _mm_or_si128(_mm_slli_si128(lo, 1), hi);
+
+	lo = _mm_add_epi8(lo, zerochar);
+	__m128i ones = _mm_cmpgt_epi8(lo, zerochar);
+	ones = _mm_or_si128(_mm_slli_si128(ones, 8), ones);
+	ones = _mm_or_si128(_mm_slli_si128(ones, 4), ones);
+	ones = _mm_or_si128(_mm_slli_si128(ones, 2), ones);
+	ones = _mm_or_si128(_mm_slli_si128(ones, 1), ones);
+	unsigned int onesBits = (unsigned int)_mm_movemask_epi8(ones);
+	unsigned long shift;
+	_BitScanForward(&shift, onesBits);
+	_mm_maskmoveu_si128(lo, ones, str-shift);
+	int len = 16-shift;
+	str[len] = '\0';
+	return len;
+}
+
+RANDOMIZED_TEST(U32ToString_SSE)
+{
+	char str[32];
+	char str2[32];
+	for(int i = 0; i < 100; ++i)
+	{
+		u32 m = rng.Int(0, 43);
+		m *= 100000000;
+		u32 n = rng.Int();
+		U32ToString_SSE(n+m, str);
+		U32ToString(n+m, str2);
+		assert(!strcmp(str, str2));
+	}
+}
+
+BENCHMARK(U32ToString_SSE, "SSE uint -> string conversion")
+{
+	U32ToString_SSE(i*1053928445, int_to_string);
+}
+BENCHMARK_END;
+
+#endif
 
 // Tests whether modulus+division is slower than division+multiplication. (doesn't seem to be the case)
 int U32ToString_Slow(u32 i, char *str)
@@ -54,7 +161,7 @@ int U32ToString_Slow(u32 i, char *str)
 			break;
 	}
 	*s = '\0';
-	int len = s - str;
+	ptrdiff_t len = s - str;
 	for(int i = 0; i < len/2; ++i)
 	{
 		char ch = str[i];
@@ -62,7 +169,7 @@ int U32ToString_Slow(u32 i, char *str)
 		str[len-1-i] = ch;
 	}
 
-	return s - str;
+	return (int)(s - str);
 }
 
 int IntToString(int i, char *str)
@@ -80,7 +187,7 @@ int IntToString(int i, char *str)
 	return U32ToString((u32)i, str);
 }
 
-TEST(IntToString)
+RANDOMIZED_TEST(IntToString)
 {
 	const int nums[] = { 0, 1, -1, 10, 11, -10, -11, INT_MIN, INT_MAX, (int)UINT_MAX, 42};
 
@@ -99,8 +206,6 @@ TEST(IntToString)
 	}
 }
 
-char int_to_string[64];
-
 BENCHMARK(IntToString, "custom int -> string conversion")
 {
 	IntToString(i*1053928445, int_to_string);
@@ -115,11 +220,11 @@ BENCHMARK_END;
 
 BENCHMARK(IntToString_itoa, "itoa int -> string conversion")
 {
-	itoa(i*1053928445, int_to_string, 10);
+	_itoa(i*1053928445, int_to_string, 10);
 }
 BENCHMARK_END;
 
-TEST(U32ToString)
+RANDOMIZED_TEST(U32ToString)
 {
 	const u32 nums[] = { 0, 1, (u32)-1, 10, 11, (u32)-10, (u32)-11, (u32)INT_MIN, INT_MAX, UINT_MAX, 42};
 
@@ -158,6 +263,6 @@ BENCHMARK_END;
 
 BENCHMARK(U32ToString_itoa, "itoa uint -> string conversion")
 {
-	itoa(i*1053928445, int_to_string, 10);
+	_itoa(i*1053928445, int_to_string, 10);
 }
 BENCHMARK_END;
