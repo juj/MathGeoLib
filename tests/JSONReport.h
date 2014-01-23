153d5a522a2d0dd5ff7fc54cbce139161496c721
Jukka Jylänki <jujjyl@gmail.com>
2014-01-23 15:13:14 +0200

    Add more comprehensive string backslash sanitation to test runner.

--- tests/JSONReport.h
+++ tests/JSONReport.h
@@ -290,12 +290,16 @@ public:
 		return ticks * 1000000.0 / Clock::TicksPerSec();
 	}
 
+	static std::string ConvertSlashes(std::string str)
+	{
+		std::replace(str.begin(), str.end(), '\\', '/');
+		return str;
+	}
+
 	void Report(const Test &t)
 	{
-		std::string fileStr = t.file;
-		std::replace(fileStr.begin(), fileStr.end(), '\\', '/');
 		LOG_WRITE("\t\t{ \"type\": \"%s\", \"name\": \"%s\", \"description\": \"%s\", \"file\": \"%s\", \"numTimesRun\": %d, \"numTrialsPerRun\": %d, \"numPasses\": %d, \"numFails\": %d, \"fastestTime\": %f, \"averageTime\": %f, \"slowestTime\": %f, \"fastestCycles\": %f },\n", 
-			t.isBenchmark ? "benchmark" : "test", t.name.c_str(), t.description.c_str(), fileStr.c_str(), t.numTimesRun, t.numTrialsPerRun, t.numPasses, t.numFails, 
+			t.isBenchmark ? "benchmark" : "test", ConvertSlashes(t.name).c_str(), ConvertSlashes(t.description).c_str(), ConvertSlashes(t.file).c_str(), t.numTimesRun, t.numTrialsPerRun, t.numPasses, t.numFails, 
 			TicksToMicroseconds(t.fastestTime), TicksToMicroseconds(t.averageTime), TicksToMicroseconds(t.worstTime), t.fastestCycles);
 		if (handle)
 			fflush(handle);
