ee29db084ed810a160520d088511aba873caba96
Jukka Jylänki <jujjyl@gmail.com>
2014-01-23 13:45:21 +0200

    Fix library_system.js to not throw when run under node.js.

--- src/emscripten/library_system.js
+++ src/emscripten/library_system.js
@@ -1,24 +1,30 @@
 mergeInto(LibraryManager.library, {
-
     browser_info: function () {
         idstr = "";
-        if (navigator.vendor && navigator.vendor.length > 0)
-            idstr += navigator.vendor + " ";
-        if (navigator.platform && navigator.platform.length > 0)
-            idstr += navigator.platform + " ";
-        if (navigator.cpuClass && navigator.cpuClass.length > 0)
-            idstr += navigator.cpuClass + " ";
-        if (navigator.appName && navigator.appName.length > 0)
-            idstr += navigator.appName + " ";
-        if (navigator.userAgent && navigator.userAgent.length > 0)
-            idstr += navigator.userAgent + " ";
-        if (navigator.systemLanguage && navigator.systemLanguage.length > 0)
-            idstr += navigator.systemLanguage + " ";
-        else if (navigator.userLanguage && navigator.userLanguage.length > 0)
-            idstr += navigator.userLanguage + " ";
-        else if (navigator.language && navigator.language.length > 0)
-            idstr += navigator.language + " ";
-
+        if (navigator) {
+          if (navigator.vendor && navigator.vendor.length > 0)
+              idstr += navigator.vendor + " ";
+          if (navigator.platform && navigator.platform.length > 0)
+              idstr += navigator.platform + " ";
+          if (navigator.cpuClass && navigator.cpuClass.length > 0)
+              idstr += navigator.cpuClass + " ";
+          if (navigator.appName && navigator.appName.length > 0)
+              idstr += navigator.appName + " ";
+          if (navigator.userAgent && navigator.userAgent.length > 0)
+              idstr += navigator.userAgent + " ";
+          if (navigator.systemLanguage && navigator.systemLanguage.length > 0)
+              idstr += navigator.systemLanguage + " ";
+          else if (navigator.userLanguage && navigator.userLanguage.length > 0)
+              idstr += navigator.userLanguage + " ";
+          else if (navigator.language && navigator.language.length > 0)
+              idstr += navigator.language + " ";
+        } else {
+          if (typeof module !== 'undefined' && module.exports) {
+            idstr = "Running in node.js VM";
+          } else {
+            idstr = "Unknown environment!";
+          }
+        }
         return allocate(intArrayFromString(idstr.trim()), 'i8', ALLOC_STACK);
     }
 
