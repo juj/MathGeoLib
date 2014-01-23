7aca6a573bc576deeb3e8a2bd8b0999c2fa0e212
Jukka Jylänki <jujjyl@gmail.com>
2014-01-23 13:48:06 +0200

    Fix previous mingw launch batch file.

--- mingw/mingw-run-tests.bat
+++ mingw/mingw-run-tests.bat
@@ -4,7 +4,7 @@ del /Q /S *.gcda > NUL 2> NUL
 del /Q *.gcno > NUL 2> NUL
 
 :: Pass --exit0 so that the execution does not cause to throw - we don't want to report test failure from code coverage analysis runs.
-MathGeoLib.exe --exit0
+MathGeoLib.exe 100 100 --exit0
 SET ERRORLEV=%ERRORLEVEL%
 
 for /R %%x in (*.gcda, *.gcno) do copy %%x . > NUL
