52a44f3e5f632ca660b581ed731fd6513fa21372
Jukka Jylänki <jujjyl@gmail.com>
2014-02-17 12:43:08 +0200

    Migrate to checking for Linux OS in the test runner via CMake and not via an explicit command line variable. Closes #24. Thanks Zao!

--- linux/cmake_release-tests.bash
+++ linux/cmake_release-tests.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm CMakeCache.txt
-cmake -DMATH_TESTS_EXECUTABLE=1 -DFAIL_USING_EXCEPTIONS=1 -DLINUX=1 -DCMAKE_BUILD_TYPE=Release $* -G "Unix Makefiles" ..
+cmake -DMATH_TESTS_EXECUTABLE=1 -DFAIL_USING_EXCEPTIONS=1 -DCMAKE_BUILD_TYPE=Release $* -G "Unix Makefiles" ..
 
 echo "make --version"
 make --version
