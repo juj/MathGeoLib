6f1fb3cf1555c11c7170734fd8ed6be3d5a27f21
Jukka Jylänki <jujjyl@gmail.com>
2014-03-08 21:57:44 +0200

    Fix build when vec==float3.

--- src/Algorithm/GJK.h
+++ src/Algorithm/GJK.h
@@ -40,7 +40,9 @@ bool GJKIntersect(const A &a, const B &b)
 	{
 		// Compute the extreme point to the direction d in the Minkowski set shape.
 		vec newSupport = SUPPORT(d);
+#ifdef MATH_VEC_IS_FLOAT4
 		assume(newSupport.w == 0.f);
+#endif
 		// If the most extreme point in that search direction did not walk past the origin, then the origin cannot be contained in the Minkowski
 		// convex shape, and the two convex objects a and b do not share a common point - no intersection!
 		if (minS + maxS < 0.f)
