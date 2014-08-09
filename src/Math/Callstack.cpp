6a37809c2b28e15c740a6a0296b4eb30f038f875
Jukka Jylänki <jujjyl@gmail.com>
2014-08-09 21:08:57 +0300

    Clean up mismatching printf specifier warning in Windows implementation for Callstack printing, as reported by VS /analyze.

--- src/Math/Callstack.cpp
+++ src/Math/Callstack.cpp
@@ -29,7 +29,7 @@ static std::string GetSymbolName(void *address, HANDLE hProcess)
 		{
 			// SymGetLineFromAddr64 returned success
 			char str[128];
-			sprintf(str, ":%d: ", line.LineNumber);
+			sprintf(str, ":%u: ", line.LineNumber);
 			return std::string(line.FileName) + str + pSymbol->Name;
 		}
 		else
