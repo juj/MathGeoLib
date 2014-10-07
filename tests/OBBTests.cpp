fa17730ddeddb30a6f4cb3914e2f617d319c8cdf
Jukka Jylänki <jujjyl@gmail.com>
2014-10-07 23:12:36 +0300

    Add a benchmark of how much computing the center of mass of a OBB-OBB intersection takes.

--- tests/OBBTests.cpp
+++ tests/OBBTests.cpp
@@ -18,4 +18,16 @@ BENCHMARK(OBBContains, "OBB::Contains(point)")
 }
 BENCHMARK_END
 
+BENCHMARK(OBBOBBIntersectionCentroid, "Centroid of OBB-OBB intersection")
+{
+	// Best: 43.610 usecs / 74139.1 ticks, Avg: 45.376 usecs, Worst: 52.899 usecs
+	const OBB &a = obb[i];
+	OBB b = obb[i+1];
+	b.pos = a.pos; // Make sure the two OBBs intersect.
+	PBVolume<12> intersection = a.ToPBVolume().SetIntersection(b.ToPBVolume());
+	Polyhedron Volume = intersection.ToPolyhedron();
+	ve[i] = Volume.ConvexCentroid();
+}
+BENCHMARK_END
+
 MATH_END_NAMESPACE
