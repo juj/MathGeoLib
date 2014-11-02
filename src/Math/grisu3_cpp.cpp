6cd9d4ca6b461a886ac93b46fe1a714c65ca0a3b
Jukka Jylänki <jujjyl@gmail.com>
2014-11-02 17:09:52 +0200

    Add my adaptation of the grisu3 double to string conversion algorithm to quickly print doubles in short forms.

---
+++ src/Math/grisu3_cpp.cpp
@@ -0,0 +1,20 @@
+/* This file is part of an implementation of the "grisu3" double to string
+	conversion algorithm described in the research paper
+
+	"Printing Floating-Point Numbers Quickly And Accurately with Integers"
+	by Florian Loitsch, available at
+	http://www.cs.tufts.edu/~nr/cs257/archive/florian-loitsch/printf.pdf */
+
+#include "grisu3.h"
+#include <string>
+#include <assert.h>
+
+std::string dtoa_grisu3_string(double v)
+{
+	char str[32];
+	int len = dtoa_grisu3(v, str);
+	assert(len > 0 && len < 25);
+	assert(str[len] == '\0');
+	((void)len);
+	return str;
+}
