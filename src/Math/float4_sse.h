0605f139226e3d10806d7c47e9a8f87dc8d703f3
Jukka Jylänki <jujjyl@gmail.com>
2014-11-10 19:56:16 +0200

    Optimize away a redundant shuffle in basis_ps (need only 5 instead of 6).

--- src/Math/float4_sse.h
+++ src/Math/float4_sse.h
@@ -148,15 +148,13 @@ FORCE_INLINE void basis_ps(simd4f v, simd4f *outB, simd4f *outC)
 	// Choose from (1,0,0), (0,1,0), and (0,0,1) the one that's most perpendicular to this vector.
 	simd4f q = and_ps(a, set_ps(0.f, 1.f, 1.f, 1.f));
 
-	// Compute (this cross q) and (this cross (this cross q)) in one go.
-	simd4f v_xzy = shuffle1_ps(v, _MM_SHUFFLE(3, 0, 2, 1)); // v_xzy = [a.w, a.x, a.z, a.y]
-	simd4f v_yxz = shuffle1_ps(v, _MM_SHUFFLE(3, 1, 0, 2)); // v_yxz = [a.w, a.y, a.x, a.z]
-	simd4f q_yxz = shuffle1_ps(q, _MM_SHUFFLE(3, 1, 0, 2)); // q_yxz = [b.w, b.y, b.x, b.z]
-	simd4f q_xzy = shuffle1_ps(q, _MM_SHUFFLE(3, 0, 2, 1)); // q_xzy = [b.w, b.x, b.z, b.y]
-	simd4f b = sub_ps(mul_ps(v_xzy, q_yxz), mul_ps(v_yxz, q_xzy));
-	simd4f a_yxz = shuffle1_ps(b, _MM_SHUFFLE(3, 1, 0, 2));
-	simd4f a_xzy = shuffle1_ps(b, _MM_SHUFFLE(3, 0, 2, 1));
-	simd4f c = sub_ps(mul_ps(v_xzy, a_yxz), mul_ps(v_yxz, a_xzy));
+	simd4f v_xzy = shuffle1_ps(v, _MM_SHUFFLE(3, 0, 2, 1));
+	simd4f q_xzy = shuffle1_ps(q, _MM_SHUFFLE(3, 0, 2, 1));
+	simd4f b_yxz = sub_ps(mul_ps(q_xzy, v), mul_ps(v_xzy, q));
+	simd4f b = shuffle1_ps(b_yxz, _MM_SHUFFLE(3, 0, 2, 1));
+	simd4f b_xzy = shuffle1_ps(b, _MM_SHUFFLE(3, 0, 2, 1));
+	simd4f c_yxz = sub_ps(mul_ps(b_xzy, v), mul_ps(v_xzy, b));
+	simd4f c = shuffle1_ps(c_yxz, _MM_SHUFFLE(3, 0, 2, 1));
 
 	*outB = mul_ps(b, rsqrt_ps(dot4_ps(b, b)));
 	*outC = mul_ps(c, rsqrt_ps(dot4_ps(c, c)));
