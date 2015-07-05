89d37a548b9dd3b1e02286c3ec7761ae6a221e3d
David Avedissian <avedissian.david@gmail.com>
2015-07-05 19:30:50 +0100

    Replaced spaces with tabs

--- src/Math/float4d.h
+++ src/Math/float4d.h
@@ -42,7 +42,7 @@ public:
 
 	float4d(){}
 	float4d(double x, double y, double z, double w):x(x), y(y), z(z), w(w) {}
-    float4d(const MATH_NS::float4 &rhs) :x(rhs.x), y(rhs.y), z(rhs.z), w(rhs.w) {}
+	float4d(const MATH_NS::float4 &rhs) :x(rhs.x), y(rhs.y), z(rhs.z), w(rhs.w) {}
 
 	double Dot(const float4d &rhs) const
 	{
@@ -149,14 +149,14 @@ public:
 #endif
 	}
 
-    MATH_NS::float4 ToFloat4() const
+	MATH_NS::float4 ToFloat4() const
 	{
 #if defined(MATH_SSE2) && defined(MATH_AUTOMATIC_SSE)
 		simd4f lo = _mm_cvtpd_ps(v[0]);
 		simd4f hi = _mm_cvtpd_ps(v[1]);
 		return _mm_movelh_ps(lo, hi);
 #else
-        return MATH_NS::float4((float)x, (float)y, float(z), float(w));
+		return MATH_NS::float4((float)x, (float)y, float(z), float(w));
 #endif
 	}
 
