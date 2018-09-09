8c12027821a4b7b1374fcce0e9bf0b2598585766
Jukka Jylänki <jujjyl@gmail.com>
2018-09-09 11:10:21 +0300

    Add generic MiniFloat routines

--- src/Math/MiniFloat.cpp
+++ src/Math/MiniFloat.cpp
@@ -1,4 +1,5 @@
 #include "MiniFloat.h"
+#include <assert.h>
 
 float Float16ToFloat32(uint16_t float16)
 {
@@ -91,4 +92,86 @@ uint16_t Float32ToFloat16(float float32)
 		newMantissa = 1; // Set the mantissa to nonzero to denote a NaN (and don't set the MSB of mantissa, to treat it as SNaN)
 
 	return (uint16_t)(sign | (newBiasedExponent << 10) | newMantissa);
+}
+
+uint32_t Float32ToMiniFloat(bool signBit, int exponentBits, int mantissaBits, int exponentBias, float value)
+{
+	assert(sizeof(float) == 4);
+	assert(exponentBits > 0);
+	assert(exponentBits <= 8);
+	assert(mantissaBits > 0);
+	assert(mantissaBits <= 23);
+	uint32_t v = *(uint32_t*)&value;
+	uint32_t biasedExponent = (v & 0x7F800000) >> 23;
+	uint32_t mantissa = v & 0x7FFFFF;
+	bool sign = (v & 0x80000000) != 0; // If true, the float is negative.
+
+	uint32_t miniFloat = 0;
+	// Write the sign bit, if sending out a signed minifloat. Otherwise, clamp all negative numbers to +zero.
+	if (signBit && sign)
+		miniFloat |= (1U << (exponentBits + mantissaBits));
+	else if (sign && biasedExponent != 0)
+		biasedExponent = mantissa = 0; // If the number was not a NaN, write out +zero.
+
+	// The maximum biased exponent value in the reduced precision representation. This corresponds to NaNs and +/-Infs.
+	const uint32_t maxBiasedExponent = (1 << exponentBits) - 1;
+
+	int trueExponent = biasedExponent - 127; // The true exponent of the float, if this number is a normal number.
+	int newBiasedExponent;
+
+	// Compute the new biased exponent value to send.
+	if (biasedExponent != 0xFF && biasedExponent != 0) // Is this a normalized float?
+	{
+		newBiasedExponent = trueExponent + exponentBias;
+		
+		// Check if the new biased exponent is too large to be represented, and the float overflows to a +/-Inf.
+		if (newBiasedExponent >= (int)maxBiasedExponent)
+		{
+			newBiasedExponent = maxBiasedExponent;
+			mantissa = 0; // To specify that this is an Inf and not a NaN.
+		}
+		// Check if the new biased exponent underflowed. In that case flush to zero.
+		///\todo This is not absolutely correct with respect to denormalized numbers. Underflowing
+		/// the exponent should produce a denormalized number, but this directly makes it zero.
+		if (newBiasedExponent <= 0)
+			newBiasedExponent = mantissa = 0;
+	}
+	else
+		newBiasedExponent = biasedExponent; // either all zeroes (+/-zero or denormal) or all ones (nan or inf).
+
+	// Scrap the given number of precision from the mantissa.
+	uint32_t newMantissa = mantissa >> (23 - mantissaBits);
+
+	// If the float was a SNaN, make sure it stays a SNaN after some of the NaN payload was removed.
+	if (biasedExponent == 0xFF && mantissa != 0 && newMantissa == 0)
+		newMantissa = 1; // Set the mantissa to nonzero to denote a NaN (and don't set the MSB of mantissa, to treat it as SNaN)
+
+	return miniFloat | (newBiasedExponent << mantissaBits) | newMantissa;
+}
+
+float MiniFloatToFloat32(bool signBit, int exponentBits, int mantissaBits, int exponentBias, uint32_t miniFloat)
+{
+	assert(sizeof(float) == 4);
+	assert(exponentBits > 0);
+	assert(exponentBits <= 8);
+	assert(mantissaBits > 0);
+	assert(mantissaBits <= 23);
+
+	uint32_t sign = signBit ? (miniFloat & (1U << (exponentBits + mantissaBits))) : 0;
+	uint32_t exponent = (miniFloat >> mantissaBits) & ((1U << exponentBits) - 1);
+	uint32_t mantissa = miniFloat & ((1U << mantissaBits) - 1);
+
+	// Shift back the decoded mantissa to proper position.
+	mantissa <<= 23 - mantissaBits;
+
+	// Reconstruct the float exponent.
+	if (exponent == (uint32_t)((1 << exponentBits) - 1)) // If the read exponent was all ones, reconstruct 11111111.
+		exponent = 0xFF;
+	else if (exponent != 0) // If the read exponent was not zero, it was a normal number.
+		exponent = exponent - exponentBias + 127;
+	// else exponent == 0, meaning a zero or a denormal.
+
+	uint32_t value = (sign ? 0x80000000U : 0) | (exponent << 23) | mantissa;
+
+	return *(float*)&value;
 }
--- src/Math/MiniFloat.h
+++ src/Math/MiniFloat.h
@@ -4,3 +4,6 @@
 
 float Float16ToFloat32(uint16_t float16);
 uint16_t Float32ToFloat16(float float32);
+
+uint32_t Float32ToMiniFloat(bool signBit, int exponentBits, int mantissaBits, int exponentBias, float value);
+float MiniFloatToFloat32(bool signBit, int exponentBits, int mantissaBits, int exponentBias, uint32_t value);
